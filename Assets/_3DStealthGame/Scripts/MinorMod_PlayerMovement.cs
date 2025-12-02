24d9a5f48d543ae6eb89d097f58c3d5c54bd3302
rj!! <pokemon102805@gmail.com>
2025-12-01 21:01:39 -0500

    Made a test scene to mess around with code

---
+++ Assets/_3DStealthGame/Scripts/MinorMod_PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MinorMod_PlayerMovement : MonoBehaviour
+{
+    Animator m_Animator;
+
+    public InputAction MoveAction;
+    public InputAction SpeedAction;
+
+    public float walkSpeed = 1.0f;
+    public float turnSpeed = 20f;
+
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        MoveAction.Enable();
+    }
+
+    void FixedUpdate()
+    {
+        var pos = MoveAction.ReadValue<Vector2>();
+        
+        if (Input.GetKeyDown("Shift"))
+        {
+            Debug.Log("Shift was pressed");
+            //walkSpeed = 1.5f;
+            //turnSpeed = 25f;
+        }
+        
+        float horizontal = pos.x;
+        float vertical = pos.y;
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+       
+
+       
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+
+        m_Rigidbody.MoveRotation(m_Rotation);
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * walkSpeed * Time.deltaTime);
+    }
+}
